1f216122d1d390e86e80506429f722849ddcd330
vipin2016 <vipinmishra138@gmail.com>
2018-05-21 12:24:04 +0530

    Initial commit

---
+++ mylibrary/src/main/java/com/sume/study/mylibrary/MyUtils.java
@@ -0,0 +1,18 @@
+package com.sume.study.mylibrary;
+
+public class MyUtils {
+
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int minus(int a, int b) {
+        return a - b;
+    }
+
+    public static int multi(int a, int b) {
+        return a * b;
+    }
+
+}
